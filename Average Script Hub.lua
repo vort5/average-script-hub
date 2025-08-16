22f236609c3da056cc9b5b68fe7e0a111e3c73c8
vort5 <yiuanamir@gmail.com>
2025-08-16 23:06:12 +0800

    Update and rename Vortex Scripts.lua to Average Script Hub.lua

--- Average Script Hub.lua
+++ Average Script Hub.lua
@@ -1,12 +1,11 @@
-local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/vort5/VortexNotif/main/source.lua"))()
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Notification-Library/main/Library.lua"))()
+
 local id = game.PlaceId
 
 if id == 4585104535 or id == 4865087807 or id == 6823014939 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vort5/Vortex-Scripts/refs/heads/main/shinobistormscript.lua"))()
-elseif id == 3082130015 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vort5/Vortex-Scripts/refs/heads/main/ninjamagicscript.lua"))()
 elseif id == 7559074529 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vort5/Vortex-Scripts/refs/heads/main/squidgamexscript.lua"))()
 else
-    NotificationLibrary:Notify("Error", "Game not supported! / Get in Game", 10)
+    NotificationLibrary:SendNotification("Error", "Game not supported! / Get in Game", 10)
 end
